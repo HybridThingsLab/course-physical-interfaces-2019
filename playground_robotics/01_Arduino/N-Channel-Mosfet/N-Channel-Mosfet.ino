5f6c511eb61e772ce72627b579dd1e3c3f628522
Andreas Muxel <login@andreasmuxel.com>
2019-12-04 15:21:18 +0100

    no message

--- playground_robotics/01_Arduino/N-Channel-Mosfet/N-Channel-Mosfet.ino
+++ playground_robotics/01_Arduino/N-Channel-Mosfet/N-Channel-Mosfet.ino
@@ -2,19 +2,28 @@
 
 void setup() {
 
+
+
 }
 
 void loop() {
 
-  int speed = 1;
+  analogWrite(3,255); // 0-255 = brightness
 
-  // put your main code here, to run repeatedly:
-  for (int i = 0; i < 255; i++) {
+  /*
+
+    int speed = 1;
+
+    // put your main code here, to run repeatedly:
+    for (int i = 0; i < 255; i++) {
     analogWrite(3, i);
     delay(speed);
-  }
-  for (int i = 255; i > 0; i--) {
+    }
+    for (int i = 255; i > 0; i--) {
     analogWrite(3, i);
     delay(speed);
-  }
+    }
+
+  */
+
 }
