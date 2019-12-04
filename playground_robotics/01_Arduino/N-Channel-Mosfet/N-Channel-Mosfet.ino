b06e2efebdd1e5d8cd543ebf654f5f3f80ac5674
Andreas Muxel <login@andreasmuxel.com>
2019-12-04 08:59:32 +0100

    added example MOSFET

---
+++ playground_robotics/01_Arduino/N-Channel-Mosfet/N-Channel-Mosfet.ino
@@ -0,0 +1,20 @@
+// instructions: https://bildr.org/2012/03/rfp30n06le-arduino/
+
+void setup() {
+
+}
+
+void loop() {
+
+  int speed = 1;
+
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 255; i++) {
+    analogWrite(3, i);
+    delay(speed);
+  }
+  for (int i = 255; i > 0; i--) {
+    analogWrite(3, i);
+    delay(speed);
+  }
+}
